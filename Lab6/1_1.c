6bbf345f9de86464fe62ae3f8d742a494af3121e
alelyya <alelyya@gmail.com>
2018-12-16 17:33:43 +0300

    1.1 - подсчёт количества переменных окружения

---
+++ Lab6/1_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern char **environ;
+int main(int argc, char *argv[])
+{
+    char **p;
+    int i = 0;
+    for (p = environ; *p != NULL; p++)
+        i++;
+    printf("%d\n", i);
+}
+
+// env | wc -l
+
